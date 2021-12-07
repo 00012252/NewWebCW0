de25579cd7a0e414b1bba3a3257fd22c2cdad58e
00012252 <00012252wiut@gmail.com>
2021-12-08 00:42:39 +0500

    added comments to suit the marking scheme requirments

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-window.onload = () => {
-  setTimeout(() => {
+window.onload = () => {     //window.onload processes load events after the element has finished loading.    
+  setTimeout(() => {               //setTimeout executes once after a specified interval 
     document.querySelector("body").classList.add("display");
   }, 4000);
 };
 
-document.querySelector(".hamburger-menu").addEventListener("click", () => {
+document.querySelector(".hamburger-menu").addEventListener("click", () => {         //if hamburger menu is clicked
+  document.querySelector(".container").classList.toggle("change");                    //the class.toggle will change and the menu will open
+});
+
+document.querySelector(".banner button").addEventListener("click", () => {          
   document.querySelector(".container").classList.toggle("change");
 });
 
-document.querySelector(".banner button").addEventListener("click", () => {
-  document.querySelector(".container").classList.toggle("change");
-});
-
-document.querySelector(".scroll-btn").addEventListener("click", () => {
-  document.querySelector("html").style.scrollBehavior = "smooth";
+document.querySelector(".scroll-btn").addEventListener("click", () => {           //scrolling button will activate when clicked and scroll to top
+  document.querySelector("html").style.scrollBehavior = "smooth";                                             //scroll to top smoothly
   setTimeout(() => {
     document.querySelector("html").style.scrollBehavior = "unset";
   }, 1000);
--- script_contact.js
+++ script_contact.js
@@ -1,4 +1,6 @@
+// this function vallidates the contact form
 function validate(){
+  // getting the information
     var name = document.getElementById("name").value;
     var subject = document.getElementById("subject").value;
     var phone = document.getElementById("phone").value;
@@ -9,8 +11,8 @@ function validate(){
     error_message.style.padding = "10px";
     
     var text;
-    if(name.length < 5){
-      text = "Please Enter valid Name";
+    if(name.length < 5){                  //if the name and surname is less than 5 symnols long
+      text = "Please Enter valid Name";     //error message will appear
       error_message.innerHTML = text;
       return false;
     }
@@ -19,21 +21,21 @@ function validate(){
       error_message.innerHTML = text;
       return false;
     }
-    if(isNaN(phone) || phone.length != 9){
-      text = "Please Enter valid Phone Number";
+    if(isNaN(phone) || phone.length != 9){          //phone numbers in uzbekistan are ususally 9 numbers long without the (+998)
+      text = "Please Enter valid Phone Number"; // isNaN checks checks if the values entered are NOT numbers, then checks if 9 numbers are entered
       error_message.innerHTML = text;
       return false;
     }
-    if(email.indexOf("@") == -1 || email.length < 6){
+    if(email.indexOf("@") == -1 || email.length < 6){       //looking for the @ sign
       text = "Please Enter valid Email";
       error_message.innerHTML = text;
       return false;
     }
-    if(message.length <= 60){
+    if(message.length <= 60){                       //messege must be at least 60 characters long
       text = "Please enter atleast 60 characters in your message";
       error_message.innerHTML = text;
       return false;
     }
-    alert("Form Submitted Successfully!");
+    alert("Form Submitted Successfully!");      //informing the user of successfylly filling the form
     return true;
   }
